1922d1bd79440c2c508ca6c9b3c596d78cfa8810
omar <omaranli56@gmail.com>
2026-01-25 23:35:56 +0400

    feat: created all models and applied migrations

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_quill.fields import QuillField
+import uuid
 
-# Create your models here.
+class DailyReviewSkill(models.Model):
+    skill = models.ForeignKey('tasks.Skill', related_name="daily_reviews_skill", on_delete=models.CASCADE)
+    daily_review = models.ForeignKey('reviews.DailyReview', related_name="daily_reviews_skill", on_delete=models.CASCADE)
+    time_spent = models.DecimalField(
+        verbose_name=_("Time spent in hours"),
+        max_digits=5,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        return f"{self.skill.name} - {self.daily_review.created_at.date()} ({self.time_spent}h)"
+
+
+    class Meta:
+        unique_together = ('skill', 'daily_review')
+
+
+class DailyReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    created_at = models.DateTimeField(verbose_name=_('date created'), auto_now_add=True)
+    description = QuillField(verbose_name=_('description'), blank=True, null=True)
+    skills = models.ManyToManyField('tasks.Skill', related_name='daily_reviews_for_skill', through=DailyReviewSkill)
+
+    def __str__(self):
+        return f"Daily Review {self.created_at.date()}"
+
+
+
+class WeeklyReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    description = QuillField(verbose_name=_('description'), blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name=_('date created'), auto_now_add=True)
+    start_date = models.DateTimeField() # from
+    end_date = models.DateTimeField() # To
+    subgoals = models.ManyToManyField(
+        'tasks.SubGoal', 
+        related_name='weekly_reviews_for_subgoal', 
+    )
+
+    def __str__(self):
+        return f"Weekly Review {self.start_date.date()} → {self.end_date.date()}"
+
+
+
+MONTH_CHOICES = [
+    (1, _("January")),
+    (2, _("February")),
+    (3, _("March")),
+    (4, _("April")),
+    (5, _("May")),
+    (6, _("June")),
+    (7, _("July")),
+    (8, _("August")),
+    (9, _("September")),
+    (10, _("October")),
+    (11, _("November")),
+    (12, _("December")),
+]
+
+month = models.PositiveSmallIntegerField(choices=MONTH_CHOICES)
+
+
+class MonthlyReview(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    created_at = models.DateTimeField(verbose_name=_('date created'), auto_now_add=True)
+    month = models.PositiveSmallIntegerField(choices=MONTH_CHOICES)
+    year = models.PositiveSmallIntegerField()
+    goals = models.ManyToManyField(
+        'tasks.Goal', 
+        related_name='monthly_reviews_for_goal', 
+    )
+
+    def __str__(self):
+        month_name = dict(MONTH_CHOICES).get(self.month, "Unknown")
+        return f"Monthly Review {month_name} {self.year}"
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_quill.fields import QuillField
+import uuid
 
-# Create your models here.
+class StatusChoices(models.TextChoices):
+    PENDING = 'pending', _('Pending')
+    IN_PROGRESS = 'in_progress', _('In Progress')
+    FINISHED = 'finished', _('Finished')
+
+class Goal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="goals")
+    name = models.CharField(verbose_name=_('goal name'), max_length=50)
+    description = QuillField(verbose_name=_('description'), blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name=_('date created'), auto_now_add=True)
+    started_at = models.DateTimeField(verbose_name=_('start date'), null=True, blank=True)
+    completed_at = models.DateTimeField(verbose_name=_('end date'),  null=True, blank=True)
+    deadline_at = models.DateTimeField(verbose_name=_('deadline'), null=True, blank=True)
+    status = models.CharField(verbose_name=_('status'), max_length=20, choices=StatusChoices, default=StatusChoices.PENDING)
+
+    @property
+    def total_time_spent(self):
+        return sum(subgoal.total_time_spent for subgoal in self.subgoals.all())
+
+    class Meta:
+        db_table = 'goal'
+        unique_together = ('user', 'name')
+
+    def __str__(self):
+        return self.name
+    
+
+class SubGoal(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, related_name="subgoals")
+    name = models.CharField(verbose_name=_('subgoal name'), max_length=50)
+    description = QuillField(verbose_name=_('description'), blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name=_('date created'), auto_now_add=True)
+    started_at = models.DateTimeField(verbose_name=_('start date'), null=True, blank=True)
+    completed_at = models.DateTimeField(verbose_name=_('end date'),  null=True, blank=True)
+    deadline_at = models.DateTimeField(verbose_name=_('deadline'), null=True, blank=True)
+    status = models.CharField(verbose_name=_('status'), max_length=20, choices=StatusChoices, default=StatusChoices.PENDING)
+
+    @property
+    def total_time_spent(self):
+        return sum(skill.total_time_spent for skill in self.skills.all())
+
+    class Meta:
+        db_table = 'subgoal'
+        unique_together = ('goal', 'name')
+
+    def __str__(self):
+        return f"{self.name} ({self.goal.name})"
+    
+
+class Skill(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    subgoal = models.ForeignKey(SubGoal, on_delete=models.CASCADE, related_name="skills")
+    name = models.CharField(verbose_name=_('skill name'), max_length=50)
+    description = QuillField(verbose_name=_('description'), blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name=_('date created'), auto_now_add=True)
+    started_at = models.DateTimeField(verbose_name=_('start date'), null=True, blank=True)
+    completed_at = models.DateTimeField(verbose_name=_('end date'),  null=True, blank=True)
+    status = models.CharField(verbose_name=_('status'), max_length=20, choices=StatusChoices, default=StatusChoices.PENDING)
+    
+    @property
+    def total_time_spent(self):
+        return self.daily_reviews_skill.aggregate(
+            total=models.Sum('time_spent')
+        )['total'] or 0
+
+    class Meta:
+        db_table = 'skill'
+        unique_together = ('subgoal', 'name')
+
+    def __str__(self):
+        return f"{self.name} ({self.subgoal.name})"
+    
+
+class Resource(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('ID'))
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name="resources")
+    name = models.CharField(verbose_name=_('Resource name'), max_length=50)
+    url = models.URLField()
+
+    class Meta:
+        db_table = 'resource'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['skill', 'name', 'url'],
+                name='unique_resource_per_skill'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.name} ({self.skill.name})"
